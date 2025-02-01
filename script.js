49855c6eda3e16245d448667e1de5fc9218d00d2
dunkdoge <altalanbiju@gmail.com>
2025-02-02 01:02:44 +1100

    made screen into one display and added operator variable that updates based on operator button clicked

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var num1;
+var num2;
+var operator = "";
+
 function add(a,b){
     return a+b;
 }
@@ -14,10 +18,32 @@ function multiply(a,b){
     return a*b;
 }
 
-const topHalfDisplay = document.querySelector(".topScreen");
+const topHalfDisplay = document.querySelector(".bottomScreen");
 const numBtns = document.querySelectorAll(".numberBtns");
 numBtns.forEach((button) => {
     button.addEventListener("click", () => {
         topHalfDisplay.textContent += Number(button.textContent);
+        num1 = Number(button.textContent)
+    })
+})
+
+const operatorBtns = document.querySelectorAll(".operatorBtns")
+operatorBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        switch (button.textContent) {
+            case "+":
+                operator = "+"
+                break;
+            case "-":
+                operator = "-"
+                break;
+            case "*":
+                operator = "*"
+                break;
+            case "/":
+                operator = "/"
+                break;
+        }
+        console.log(operator);
     })
 })
